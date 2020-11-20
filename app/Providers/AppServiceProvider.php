b72d7bbdf3bea5fd307fe17f847fc4e324ff3f12
Sebatja <jsparra943@misena.edu.co>
2020-11-19 19:15:41 -0500

    2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Env;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\FACADES\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        if(env('APP_ENV') !== 'local'){
+             URL::forceScheme('https');
+        }
     }
+    
 }
