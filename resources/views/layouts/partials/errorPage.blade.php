2f8e356637f8d734cb1c173552c0c6f6b236ce53
KesGroom <kesgroom@gmail.com>
2020-11-20 09:34:33 -0500

    Links para imagenes

--- resources/views/layouts/partials/errorPage.blade.php
+++ resources/views/layouts/partials/errorPage.blade.php
@@ -83,7 +83,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('storage/colegio/escudo.jpg') }}" alt="Escudo"
+                    <img src="{{ asset('img/escudo.jpg') }}" alt="Escudo"
                         style="width: 5rem; height: 3rem;">
                     <span class="navbar-brand mb-0 h1">María Cano IED</span>
                 </a>
