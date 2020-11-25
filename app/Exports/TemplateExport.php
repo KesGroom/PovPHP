d1ee0454e4a169e11437b5808c9330fc83c993e6
KesGroom <kesgroom@gmail.com>
2020-11-25 17:32:04 -0500

    Prueba 6, excel export Global

--- app/Exports/TemplateExport.php
+++ app/Exports/TemplateExport.php
@@ -15,14 +15,14 @@ class TemplateExport implements FromView
     public $data;
     public $headers;
 
-    public function __construct($data, $headers)
+    public function __construct($headers)
     {
         $this->headers = $headers;
-        $this->data = $data;
+        // $this->data = $data;
     }
 
     public function view(): View
     {
-        return view('exports.ExportTemplate', ['data' => $this->data, 'headers' => $this->headers]);
+        return view('exports.ExportTemplate', ['headers' => $this->headers]);
     }
 }
